ae927dc5a60b2f8999dc4b7efecdbd5b99641a20
SudarshanAhire <sudarshanahire292@gmail.com>
2024-11-24 23:24:39 +0530

    commit1

--- Learn_Use_Context/react-project-nine/src/App.jsx
+++ Learn_Use_Context/react-project-nine/src/App.jsx
@@ -1,14 +1,24 @@
-
+import {createContext, useState } from 'react'
 import './App.css'
+import ChildA from './components/ChildA';
+
+//step:1  create context
+const UserContext = createContext();
+//step:2 wrap all the child inside a provider
+//step:3 pass value
+//step:4 consumer k ander jake consume karalo
 
 function App() {
+  const [user, setUser] = useState({name:"sudarshan"});
   
-
   return (
-   <div>
-    Hello jee
-   </div>
+   <>
+    <UserContext.Provider value={user}>
+      <ChildA />
+    </UserContext.Provider>
+   </>
   )
 }
 
 export default App
+export {UserContext}
