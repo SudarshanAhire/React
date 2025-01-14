23349ba09f38e5279a6efd51f3b5bbd9e7f26074
SudarshanAhire <sudarshanahire292@gmail.com>
2025-01-14 20:08:12 +0530

    commit1

--- react-form-project/src/App.jsx
+++ react-form-project/src/App.jsx
@@ -1,13 +1,34 @@
+import { useForm } from "react-hook-form"
 
 import './App.css'
 
 function App() {
- 
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   return (
-     <div>
-      Helllo jee
-     </div>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <div>
+        <label>First Name: </label>
+        <input {...register('firstName')}/>
+      </div>
+      <br/>
+      <div>
+        <label>Middle Name: </label>
+        <input {...register('middleName')}/>
+      </div>
+      <br/>
+      <div>
+        <label>Last Name: </label>
+        <input {...register('lastName')}/>
+      </div>
+      <br/>
+    </form>
   )
 }
 
